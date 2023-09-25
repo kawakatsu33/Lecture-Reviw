4c3fd4f0c9e35d428015f4674b934b7ada84d551
kawakatsu33 <jibanyan3@icloud.com>
2023-09-26 02:26:34 +0900

    subjectsマイグレーションファイルにperiodを新規追加、lectureからperiodけしてtimes追加。簡単な修正なのにファイルの重複のエラーが出過ぎて苦戦

--- database/migrations/2023_09_25_021041_subjects.php
+++ database/migrations/2023_09_25_021041_subjects.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('name',50);
+            $table->integer('period');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
